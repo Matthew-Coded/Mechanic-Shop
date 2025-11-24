9c4fb6db22c7478f62014d76330839875336402a
Matthew-Coded <7matthew.bautista7@gmail.com>
2025-11-24 12:35:14 -0800

    set up __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,15 @@ from .models import *
 
 
 
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mechanic_shop.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
